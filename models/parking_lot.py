f480bffc3acb221c2dc2f35db6a9a9f765cbbf29
Suyash Soni <suyash.soni248@gmail.com>
2019-03-11 00:23:51 +0530

    Refactoring related to models, added docs

--- models/parking_lot.py
+++ models/parking_lot.py
@@ -20,10 +20,16 @@ class ParkingLot(object):
 
     @property
     def occupied_slots(self):
+        """
+        :return: Returns a copy of `occupied_slots`
+        """
         return copy.copy(self.__occupied_slots__)
 
     @property
     def available_slots(self):
+        """
+       :return: Returns a copy of `available_slots`
+       """
         return copy.copy(self.__available_slots__)
 
     # Time complexity: O(1)
@@ -34,6 +40,10 @@ class ParkingLot(object):
         return len(self.__occupied_slots__) >= self.__size__
 
     def get_closest_slot(self):
+        """
+        Returns a closest(with minimum slot number) slot.
+        :return:
+        """
         if not self.__available_slots__:
             return None
         return min(self.__available_slots__)
@@ -45,14 +55,28 @@ class ParkingLot(object):
         return slot_number in self.__occupied_slots__
 
     def is_vehicle_parked(self, registration_number):
+        """
+        Checks if the vehicle with specified registration number is already parked.
+        :param registration_number:
+        """
         return registration_number in self.__vehicle_slot_mapping__
 
     def occupy_slot(self, slot, vehicle):
+        """
+        Reserves a `slot` for the `vehicle`
+        :param slot: `Slot` to be reserved/occupied.
+        :param vehicle: `Vehicle` to be parked.
+        """
         self.__occupied_slots__[slot.slot_number] = vehicle.registration_number, vehicle.color
         self.__vehicle_slot_mapping__[vehicle.registration_number] = slot.slot_number
         self.__available_slots__.remove(slot)
 
     def vacate_slot(self, slot, reg_num):
+        """
+        Makes the `slot` free and available to be used by other vehicles.
+        :param slot:
+        :param reg_num: Vehicle registration number.
+        """
         self.__occupied_slots__.pop(slot.slot_number)
         self.__vehicle_slot_mapping__.pop(reg_num)
         self.__available_slots__.add(slot)
