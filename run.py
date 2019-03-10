d3f5588b4fc3a75ba83895caa20ff76fb3fd1ba3
Suyash Soni <suyash.soni248@gmail.com>
2019-03-11 00:11:42 +0530

    Bootstrap script - Running application via interactive console

--- run.py
+++ run.py
@@ -1,2 +1,27 @@
 __author__ = "Suyash Soni"
 __email__ = "suyash.soni248@gmail.com"
+
+import sys
+from util.dispatcher import Dispatcher
+from util.logger import logger
+
+def run():
+    """
+    Bootstraps the application.
+    """
+    dispatcher = Dispatcher()
+
+    cmd = None
+    while True:
+        cmd = input()
+        if cmd:
+            if cmd == 'exit':
+                break
+            else:
+                response = dispatcher.dispatch(cmd)
+
+if __name__ == '__main__':
+    run()
+
+
+
