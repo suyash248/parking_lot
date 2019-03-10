eb57621c8a7c662c62ac0e1546b4515b1998ac9f
Suyash Soni <suyash.soni248@gmail.com>
2019-03-11 00:21:22 +0530

    Bootstrap script - Supplyig commands via file

--- run.py
+++ run.py
@@ -11,14 +11,24 @@ def run():
     """
     dispatcher = Dispatcher()
 
-    cmd = None
-    while True:
-        cmd = input()
-        if cmd:
-            if cmd == 'exit':
-                break
-            else:
-                response = dispatcher.dispatch(cmd)
+    input_file_path = sys.argv[1] if len(sys.argv) > 1 else None
+    if input_file_path:
+        try:
+            with open(input_file_path, "r") as f:
+                for cmd in f:
+                    response = dispatcher.dispatch(cmd)
+        except FileNotFoundError:
+            logger.error("No such file: {}".format(input_file_path))
+
+    else:
+        cmd = None
+        while True:
+            cmd = input()
+            if cmd:
+                if cmd == 'exit':
+                    break
+                else:
+                    response = dispatcher.dispatch(cmd)
 
 if __name__ == '__main__':
     run()
